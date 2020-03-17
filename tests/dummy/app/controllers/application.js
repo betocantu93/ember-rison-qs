a0edb5c8031139c0949d382c1b54515368f84723
betocantu93 <betocantu93@gmail.com>
2020-03-17 14:34:41 -0600

    update dummy

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,14 +1,41 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-	queryParams = ['query'];
-	
-	query = {
-		hello: 'World',
-		how: {
-			are: {
-				you: 'fine'
+	queryParams = ['oneQueryParam'];
+
+	@tracked oneQueryParam = 3;
+
+	@tracked partialState = {
+		amazing: ['super', 'cool']
+	}
+
+	@tracked num = this.oneQueryParam?.some?.hard?.num || 0;
+
+	get someHardState() {
+		return {
+			some: {
+				hard: {
+					state: ['super', {
+						hard: true,
+						state: ['inside', true, 'of this']
+					}],
+					...this.partialState,
+					num: this.num
+				}
 			}
 		}
 	}
+
+	@action
+	incrementor() {
+		this.num++;
+	}
+
+	@action
+	changeQuery() {
+		this.set('partialState', { prop: 1, other: { complex: 'stuff'}});
+	}
 }
+
